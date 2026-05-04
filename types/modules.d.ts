f7c2061f5c91d28c9f1cd88071d5e33884d92041
abusufyan-netizen <safi4730358@gmail.com>
2026-05-04 22:55:31 +0500

    Fix all missing module declarations (remark, gray-matter, remark-html)

---
+++ types/modules.d.ts
@@ -0,0 +1,14 @@
+declare module 'remark-html' {
+  const html: any;
+  export default html;
+}
+
+declare module 'remark' {
+  const remark: any;
+  export { remark };
+}
+
+declare module 'gray-matter' {
+  const matter: any;
+  export default matter;
+}
