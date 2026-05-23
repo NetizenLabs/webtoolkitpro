b461ad21226b05ed7caa58866530f59191d2d2d4
abusufyan-netizen <safi4730358@gmail.com>
2026-05-23 08:44:13 +0500

    fix: resolve sw navigation crash and adsense iframe block

--- app/sw.ts
+++ app/sw.ts
@@ -19,7 +19,6 @@ const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
-  navigationPreload: true,
   runtimeCaching: defaultCache,
 });
 
