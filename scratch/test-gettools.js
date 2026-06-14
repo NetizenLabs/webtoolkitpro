689a9568cecd1e2fb08637ab3f3b4ed4e854e8d7
abusufyan-netizen <safi4730358@gmail.com>
2026-06-14 18:58:12 +0500

    chore: cleaned up unused temp files, scripts, and logs, updated tradeconvert submodule

--- scratch/test-gettools.js
+++
@@ -1,7 +0,0 @@
-require('ts-node').register({ transpileOnly: true });
-const { getTools } = require('./lib/tools');
-const tools = getTools();
-console.log('Total tools:', tools.length);
-console.log('Tools not coming soon:', tools.filter(t => !t.isComingSoon).length);
-console.log('Tools coming soon:', tools.filter(t => t.isComingSoon).length);
-console.log('First 5 coming soon:', tools.filter(t => t.isComingSoon).slice(0, 5).map(t => t.slug));
